d9d074f591524f933fe83723eb4bb0557f1f69dc
Evan <evanmarkert@gmail.com>
2022-04-21 15:50:16 -0400

    add HTML/CSS templates

---
+++ app.js
@@ -0,0 +1,19 @@
+let myVinyl = []
+
+function Vinyl(title, artist, year, wishlist) {
+    this.title = title
+    this.artist = artist
+    this.year = year
+    this.wishlist = wishlist
+}
+
+function addVinylToCollection() {
+
+}
+
+
+//UI
+
+const addBtn = document.getElementById('addRecord')
+
+
